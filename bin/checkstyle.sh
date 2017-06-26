a7b558ec30eefc2c968eacc6a8c7c1eb3f96a1d0
Pin Zhang <zhangpin04@gmail.com>
2017-06-26 21:00:59 +0800

    Fix code style by using php-cs-fixer

---
+++ bin/checkstyle.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+#
+
+for file in `find ./application -name *.php -not -path "*/config/*"`;
+do
+  if [ "${file##*.}" = "php" ]; then
+    echo "Check if $file compliants to psr2 .."
+    ./vendor/bin/php-cs-fixer fix "$file" -v --dry-run
+    exit_code=$?
+    if [ $exit_code != 0 ]; then
+      echo "$file is not compliant to psr, please fix it, $exit_code."
+      exit $exit_code
+    fi
+  fi
+done
+
+exit 0
+
